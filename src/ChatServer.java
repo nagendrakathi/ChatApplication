341237df166a96bbff60246fd0e5c95d697ee5f8
Aslesh <nagendra30244@gmail.com>
2024-10-24 08:03:18 +0530

    modify

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -7,7 +7,7 @@ public class ChatServer {
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(1234)) {
             System.out.println("Server is listening on port 1234...");
-
+            
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
